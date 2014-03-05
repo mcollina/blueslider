b44ea9197ebf300dfc7d7a645f1d4391d2d96c3f
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2014-03-05 17:19:01 +0100

    added mouse support

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ if (!process.env.DEBUG)
 
 var debug = require('debug')('blue')
   , SensorTag = require('sensortag')
-  , press = require('mac-key-press')
+  , press = require('mac-key-press').press
+  , move = require('mac-key-press').move
+  , getPos = require('mac-key-press').getPos
   , leftKey = 123
   , rightKey = 124
+  , accelPeriod = 50
 
 debug('searching for a sensor tag')
 
@@ -18,10 +21,20 @@ SensorTag.discover(function(sensorTag) {
     sensorTag.discoverServicesAndCharacteristics(function() {
       debug('discovered all characteristics')
       sensorTag.notifySimpleKey(function() {
-        debug('notify correctly setted up')
+        debug('simplekey notify correctly set up')
+      });
+      sensorTag.setAccelerometerPeriod(accelPeriod, function(){
+        debug('accelerometer period set to', accelPeriod)
+      });
+      sensorTag.enableAccelerometer(function() {
+        debug('accelerometer enabled')
+      });
+      sensorTag.notifyAccelerometer(function() {
+        debug('accelerometer notify correctly set up')
       })
     });
   })
+
 
   sensorTag.on('simpleKeyChange', function(left, right) {
     if (left) {
@@ -32,5 +45,13 @@ SensorTag.discover(function(sensorTag) {
       press(rightKey)
       press(36) // return
     }
+  });
+
+  sensorTag.on('accelerometerChange', function(x, y, z){
+    //debug('accelerometer:',x, y, z);
+    cursor = getPos();
+    move(cursor.x + 500*x, cursor.y + 500*y);
   })
 })
+
+
